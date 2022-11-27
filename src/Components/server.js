2c0cf13c26d3f9f868f1ec81c10862f49d2a8d59
samaneh karimi <samanehkarimi17@gmail.com>
2022-11-27 19:02:35 +0330

    update server.js-return

--- src/Components/server.js
+++ src/Components/server.js
@@ -1,20 +1,20 @@
 
-  const usersData = [];
+
 export const getData = async () => {
-  
+    const usersData = [];
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const userData = await res.json();
   
-    userData.map(d => {
-        usersData.push({
+     userData.map(d => {
+      return  usersData.push({
             id: d.id,
             name: d.name,
             username: d.username,
             email: d.email
         })
-        return usersData;
+  
     })
-
+      return usersData;
     
 }
 
